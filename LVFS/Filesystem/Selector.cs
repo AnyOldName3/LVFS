1d8a5a3d769e6732f7cbaa850b25915ba9f408e5
krizd <krizd@PETESLAPTOP>
2016-12-16 21:37:10 +0000

    Add some documentation to Selector.

--- LVFS/Filesystem/Selector.cs
+++ LVFS/Filesystem/Selector.cs
@@ -8,32 +8,50 @@ using LVFS.Sources;
 
 namespace LVFS.Filesystem
 {
+    /// <summary>
+    /// A class responsible for fetching the Source(s) responsible for files and files available from the collection of Sources
+    /// </summary>
 	class Selector
 	{
 		private IList<Source> mSources;
 		private Source mOutputSource;
 
+        /// <summary>
+        /// Constructs a new Selector with a list of Sources
+        /// </summary>
+        /// <param name="sources">The list of Sources to use</param>
 		public Selector(IList<Source> sources)
 		{
 			mSources = new List<Source>(sources);
 			mOutputSource = mSources.Last<Source>();
 		}
 
+        /// <summary>
+        /// Constructs a new Selector with no Sources
+        /// </summary>
 		public Selector()
 		{
 			mSources = new List<Source>();
 			mOutputSource = null;
 		}
 
+        /// <summary>
+        /// Adds an additional Source to the LVFS as the highest priority Source
+        /// </summary>
+        /// <param name="source">The Source to add</param>
 		public void addSource(Source source)
 		{
 			if (source == null)
 				throw new ArgumentNullException("source");
 			mSources.Add(source);
-			if (mOutputSource == null)
-				mOutputSource = source;
+			mOutputSource = source;
 		}
 
+        /// <summary>
+        /// Gets the source responsible for a file/directory
+        /// </summary>
+        /// <param name="fileName">The path to the file/directory</param>
+        /// <returns>The Source responsible for the file/directory</returns>
 		public Source SourceOf(string fileName)
 		{
 			for (int i = mSources.Count - 1; i >= 0; i--)
