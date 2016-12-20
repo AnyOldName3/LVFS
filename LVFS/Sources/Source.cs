c2f6d5df70fb253638f3b659397d1e34395a320a
AnyOldName3 <krizdjali+github@gmail.com>
2016-12-20 20:24:22 +0000

    Add more functions and their documentation to Source. Update to work with predecessors.

--- LVFS/Sources/Source.cs
+++ LVFS/Sources/Source.cs
@@ -13,6 +13,39 @@ namespace LVFS.Sources
 	/// </summary>
 	abstract class Source
 	{
+		private Source mPredecessor;
+
+		public bool IsFirst { get { return mPredecessor == null; } }
+
+		/// <summary>
+		/// Construct a source with the specified predecessor.
+		/// </summary>
+		/// <param name="predecessor">The predecessor of the current source. It should be null if there is no predecessor.</param>
+		protected Source(Source predecessor)
+		{
+			mPredecessor = predecessor;
+		}
+
+		/// <summary>
+		/// As with ListFiles, but for the predecessor source.
+		/// </summary>
+		/// <param name="path">The path of the directory to list the contents of</param>
+		/// <returns>A list of file information structs.</returns>
+		protected IList<FileInformation> ListPredecessorFiles(string path)
+		{
+			return mPredecessor.ListFiles(path);
+		}
+
+		/// <summary>
+		/// As with GetFileInformation, but for the predecessor source.
+		/// </summary>
+		/// <param name="path">The path to get file information for</param>
+		/// <returns>A file information struct if the path corresponds to a file, or null if not.</returns>
+		protected FileInformation? GetPredecessorFileInformation(string path)
+		{
+			return mPredecessor.GetFileInformation(path);
+		}
+		
 		/// <summary>
 		/// Gets whether or not this source controls the specified file
 		/// </summary>
@@ -24,8 +57,14 @@ namespace LVFS.Sources
 		/// Lists the files and subdirectories contained within a given directory
 		/// </summary>
 		/// <param name="path">The directory to list the contents of</param>
-		/// <param name="inputFiles">The combined contents of all lower priority layers of the filesystem. This will be null if no lower level contains the requested directory.</param>
 		/// <returns>A list of files in the given directory when this source and all lower priority sources have been considered.</returns>
-		public abstract IList<FileInformation> ListFiles(string path, IList<FileInformation> inputFiles);
+		public abstract IList<FileInformation> ListFiles(string path);
+
+		/// <summary>
+		/// Gets file information for the file with the specified path (if it exists), or null otherwise.
+		/// </summary>
+		/// <param name="path">The file path to get the information for</param>
+		/// <returns>A nullable FileInformation struct for the requested file.</returns>
+		public abstract FileInformation? GetFileInformation(string path);
 	}
 }
