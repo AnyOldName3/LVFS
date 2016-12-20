3c558eba9e3b876adb8ed149b85ef2f65bc58dc7
AnyOldName3 <krizdjali+github@gmail.com>
2016-12-20 20:34:59 +0000

    Restore indentation consistency

--- LVFS/Filesystem/Selector.cs
+++ LVFS/Filesystem/Selector.cs
@@ -10,37 +10,37 @@ using LVFS.Sources;
 
 namespace LVFS.Filesystem
 {
-    /// <summary>
-    /// A class responsible for fetching the Source(s) responsible for files and files available from the collection of Sources
-    /// </summary>
+	/// <summary>
+	/// A class responsible for fetching the Source(s) responsible for files and files available from the collection of Sources
+	/// </summary>
 	class Selector
 	{
 		private IList<Source> mSources;
 		private Source mOutputSource;
 
-        /// <summary>
-        /// Constructs a new Selector with a list of Sources
-        /// </summary>
-        /// <param name="sources">The list of Sources to use</param>
+		/// <summary>
+		/// Constructs a new Selector with a list of Sources
+		/// </summary>
+		/// <param name="sources">The list of Sources to use</param>
 		public Selector(IList<Source> sources)
 		{
 			mSources = new List<Source>(sources);
 			mOutputSource = mSources.Last<Source>();
 		}
 
-        /// <summary>
-        /// Constructs a new Selector with no Sources
-        /// </summary>
+		/// <summary>
+		/// Constructs a new Selector with no Sources
+		/// </summary>
 		public Selector()
 		{
 			mSources = new List<Source>();
 			mOutputSource = null;
 		}
 
-        /// <summary>
-        /// Adds an additional Source to the LVFS as the highest priority Source
-        /// </summary>
-        /// <param name="source">The Source to add</param>
+		/// <summary>
+		/// Adds an additional Source to the LVFS as the highest priority Source
+		/// </summary>
+		/// <param name="source">The Source to add</param>
 		public void addSource(Source source)
 		{
 			if (source == null)
@@ -49,11 +49,11 @@ namespace LVFS.Filesystem
 			mOutputSource = source;
 		}
 
-        /// <summary>
-        /// Gets the source responsible for a file/directory
-        /// </summary>
-        /// <param name="fileName">The path to the file/directory</param>
-        /// <returns>The Source responsible for the file/directory</returns>
+		/// <summary>
+		/// Gets the source responsible for a file/directory
+		/// </summary>
+		/// <param name="fileName">The path to the file/directory</param>
+		/// <returns>The Source responsible for the file/directory</returns>
 		public Source SourceOf(string fileName)
 		{
 			for (int i = mSources.Count - 1; i >= 0; i--)
