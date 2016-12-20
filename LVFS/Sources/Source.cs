a082d01090782a06ba5280c914e77ccc641343e2
AnyOldName3 <krizdjali+github@gmail.com>
2016-12-20 17:22:40 +0000

    Add more functions and documentation to Source

--- LVFS/Sources/Source.cs
+++ LVFS/Sources/Source.cs
@@ -4,10 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using DokanNet;
+
 namespace LVFS.Sources
 {
+	/// <summary>
+	/// Represents a source (individual layer) in the LVFS.
+	/// </summary>
 	abstract class Source
 	{
-		public abstract bool HasFile(string fileName);
+		/// <summary>
+		/// Gets whether or not this source controls the specified file
+		/// </summary>
+		/// <param name="path">The file path being queried</param>
+		/// <returns>whether or not this source controls the specified file</returns>
+		public abstract bool ControlsFile(string path);
+
+		/// <summary>
+		/// Lists the files and subdirectories contained within a given directory
+		/// </summary>
+		/// <param name="path">The directory to list the contents of</param>
+		/// <param name="inputFiles">The combined contents of all lower priority layers of the filesystem. This will be null if no lower level contains the requested directory.</param>
+		/// <returns>A list of files in the given directory when this source and all lower priority sources have been considered.</returns>
+		public abstract IList<FileInformation> ListFiles(string path, IList<FileInformation> inputFiles);
 	}
 }
