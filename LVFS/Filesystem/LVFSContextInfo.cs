0d48ac0b8669fdbda80b9079539d160581cf301e
AnyOldName3 <krizdjali+github@gmail.com>
2017-01-07 00:54:44 +0000

    Fix major oversight with LVFSContextInfo not relaying changes to the DokanFileInfo which it wraps.

--- LVFS/Filesystem/LVFSContextInfo.cs
+++ LVFS/Filesystem/LVFSContextInfo.cs
@@ -11,9 +11,21 @@ namespace LVFS.Filesystem
 {
 	class LVFSContextInfo
 	{
+		DokanFileInfo mParent;
+
 		public IDictionary<Sources.Source, object> Context { get; private set; }
-		public bool DeleteOnClose { get; private set; }
-		public bool IsDirectory { get; private set; }
+		
+		public bool DeleteOnClose
+		{
+			get { return mParent.DeleteOnClose; }
+			set { mParent.DeleteOnClose = value; }
+		}
+		public bool IsDirectory
+		{
+			get { return mParent.IsDirectory; }
+			set { mParent.IsDirectory = value; }
+		}
+
 		public bool NoCache { get; private set; }
 		public bool PagingIo { get; private set; }
 		public int ProcessId { get; private set; }
@@ -25,9 +37,15 @@ namespace LVFS.Filesystem
 
 		public LVFSContextInfo(DokanFileInfo info)
 		{
+			mParent = info;
+
 			Context = (IDictionary<Sources.Source, object>)info.Context;
-			DeleteOnClose = info.DeleteOnClose;
-			IsDirectory = info.IsDirectory;
+			if (Context == null)
+			{
+				Context = new Dictionary<Sources.Source, object>();
+				mParent.Context = Context;
+			}
+
 			NoCache = info.NoCache;
 			PagingIo = info.PagingIo;
 			ProcessId = info.ProcessId;
