b7bc72d21664592212661677b6d457171051bbac
AnyOldName3 <krizdjali+github@gmail.com>
2017-01-07 00:56:41 +0000

    Revert "Fix bug in LVFS" as it was obsoleted by 0d48ac0b
    This reverts commit f2b028a481c3d8d73cb7de524f522b53be5db949.

--- LVFS/Filesystem/LVFS.cs
+++ LVFS/Filesystem/LVFS.cs
@@ -35,10 +35,7 @@ namespace LVFS.Filesystem
 
 		public NtStatus CreateFile(string fileName, DokanNet.FileAccess access, FileShare share, FileMode mode, FileOptions options, FileAttributes attributes, DokanFileInfo info)
 		{
-			LVFSContextInfo LVFSInfo = new LVFSContextInfo(info);
-			var result = mSelector.CreateFileHandle(fileName, access, share, mode, options, attributes, LVFSInfo);
-			info.Context = LVFSInfo.Context;
-			return result;
+			return mSelector.CreateFileHandle(fileName, access, share, mode, options, attributes, new LVFSContextInfo(info));
 		}
 
 		public NtStatus DeleteDirectory(string fileName, DokanFileInfo info)
