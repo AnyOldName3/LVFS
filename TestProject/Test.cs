dc3971e3f27c64d302196d7f77316899446c2aa6
AnyOldName3 <krizdjali+github@gmail.com>
2017-01-08 21:58:17 +0000

    Add a test project to aid investigations and debugging

---
+++ TestProject/Test.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LVFS
+{
+	class Test
+	{
+		static void SwitchThing(FileMode mode)
+		{
+			Console.WriteLine("Testing " + mode);
+			switch (mode)
+			{
+				case FileMode.Open:
+					Console.WriteLine("Open");
+					break;
+				
+				case FileMode.CreateNew:
+					Console.WriteLine("CreateNew");
+					break;
+
+				case FileMode.Truncate:
+					Console.WriteLine("Truncate");
+					break;
+			}
+			Console.WriteLine();
+		}
+
+		static void Main(string[] args)
+		{
+			SwitchThing(FileMode.Append);
+			SwitchThing(FileMode.Create);
+			SwitchThing(FileMode.CreateNew);
+			SwitchThing(FileMode.Open);
+			SwitchThing(FileMode.OpenOrCreate);
+			SwitchThing(FileMode.Truncate);
+		}
+	}
+}
